e69c1082d3043d9484069cc9677685a766ef930c
Qing Wang <chingwang.qw@gmail.com>
2019-08-19 23:00:02 +0100

    rewrite like-button alert

--- qingblog.js
+++ qingblog.js
@@ -1,9 +1,3 @@
-// document.querySelectorAll(".like-button").forEach((button) => {
-//     button.onclick = () => {
-//         alert("Thank you!")
-//     }
-// })
-
 $(document).ready(function(){
     $(".like-button").click(function(){
         alert("Thank you!");
