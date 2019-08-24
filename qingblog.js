566f483bec4aa94ce3ff433c8ea07fbc5dd6ab7d
Qing Wang <chingwang.qw@gmail.com>
2019-08-24 22:14:20 +0100

    initial night mode

--- qingblog.js
+++ qingblog.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
     $(".like-button").click(function(){
         alert("Thank you!");
     });
+
+    $(".light-switch").click(function(){
+        $("body").toggleClass("night-mode");
+    });
 });
